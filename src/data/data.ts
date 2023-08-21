15720053b45cb689f3c97ebac249ad7e29966ef8
Albert Colubi <albertmc89@gmail.com>
2023-08-21 21:12:09 +0200

    Add things data and types

---
+++ src/data/data.ts
@@ -0,0 +1,20 @@
+import { type Thing } from "./types.js";
+
+export const things: Thing[] = [
+  {
+    id: 1,
+    description: "Do sport",
+  },
+  {
+    id: 2,
+    description: "Do homework",
+  },
+  {
+    id: 3,
+    description: "Do the challenge",
+  },
+  {
+    id: 4,
+    description: "Make dinner",
+  },
+];
