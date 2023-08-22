c0f8e4d6ad3c2bcd8838fcad7e75e4ceae91b2a2
Albert Colubi <albertmc89@gmail.com>
2023-08-22 18:20:26 +0200

    Test getThings controller

---
+++ src/server/controller/things/thingsController.test.ts
@@ -0,0 +1,26 @@
+import { type Request, type Response } from "express";
+import { getThings } from "./thingsController.js";
+
+const req: Partial<Request> = {};
+const res: Partial<Response> = {
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn(),
+};
+
+describe("Given a getThings controller", () => {
+  describe("When it receives a response", () => {
+    test("Then it should call method status with 200", () => {
+      const expectedStatusCode = 200;
+
+      getThings(req as Request, res as Response);
+
+      expect(res.status).toBeCalledWith(expectedStatusCode);
+    });
+
+    test("Then it should call its method json", () => {
+      getThings(req as Request, res as Response);
+
+      expect(res.json).toHaveBeenCalledTimes(2);
+    });
+  });
+});
